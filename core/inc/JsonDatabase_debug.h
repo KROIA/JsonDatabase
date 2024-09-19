55b1b5a352565232865795fc3098da88f840e08b
Alex <alexkrieg@gmx.ch>
2024-09-19 17:45:33 +0200

    -

--- core/inc/JsonDatabase_debug.h
+++ core/inc/JsonDatabase_debug.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "JsonDatabase_global.h"
 
+/// USER_SECTION_START 14
+
+/// USER_SECTION_END
+
 // The Logger library is automaticly included if the logger dependency .cmake file is available
-#if defined(LOGGER) and not defined(LOGGER_LIB)
+#if defined(LOGGER)
 	#include "Logger.h"
 #endif
 
@@ -107,7 +111,7 @@ namespace JsonDatabase
 /// USER_SECTION_END
 
 
-#if defined(LOGGER) and not defined(LOGGER_LIB)
+#if defined(LOGGER)
 	class JSON_DATABASE_EXPORT Logger 
 	{
 		/// USER_SECTION_START 8
@@ -161,6 +165,9 @@ namespace JsonDatabase
 
 /// USER_SECTION_END
 #endif
+/// USER_SECTION_START 13
+
+/// USER_SECTION_END
 }
 
 
