4ed40bf28c292638a365231f37c126194c98eafd
Alex Krieg <alexkrieg@gmx.ch>
2024-09-19 15:24:40 +0200

    ~ Update Library Template 1.3.0

--- core/src/JsonDatabase_info.cpp
+++ core/src/JsonDatabase_info.cpp
@@ -1,5 +1,4 @@
 #include "JsonDatabase_info.h"
-#include "JsonDatabase_debug.h"
 
 /// USER_SECTION_START 1
 
@@ -151,25 +150,7 @@ namespace JsonDatabase
 	}
 #endif
 
-	// Implementation of the Profiler start/stop functions
-	void Profiler::start()
-	{
-#ifdef JD_PROFILING
-		EASY_PROFILER_ENABLE;
-#endif
-	}
-	void Profiler::stop()
-	{
-		stop("profile.prof");
-	}
-	void Profiler::stop(const char* profilerOutputFile)
-	{
-#ifdef JD_PROFILING
-		profiler::dumpBlocksToFile(profilerOutputFile);
-#else
-		(void)profilerOutputFile;
-#endif
-	}
+
 
 
 /// USER_SECTION_START 4
